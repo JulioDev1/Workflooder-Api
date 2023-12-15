26b34aac8696892164cbd5ea94609258895e4c27
Julio Cesar <juliocesargualberto617@gmail.com>
2023-12-15 12:36:10 -0300

    feature:add authorization middleware

--- src/Middleware/ensureAuthenticated.ts
+++ src/Middleware/ensureAuthenticated.ts
@@ -1,7 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { verify } from "jsonwebtoken";
 
 export function EnsureAuthenticated(
   request: FastifyRequest,
   reply: FastifyReply,
   done: (err?: Error) => void
-) {}
+) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) return reply.code(401).send({ message: "lost connection" });
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    verify(token, "5b1305ce-2409-4370-bbe4-5b201de352d3");
+    return done();
+  } catch (error) {
+    reply.code(401).send("invalid token");
+  }
+}
